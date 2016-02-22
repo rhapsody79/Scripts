40cb637a9bd93eebf7c6513c470c827d4fde4fb2
rhapsody79 <claudio_gianno79@hotmail.com>
2016-02-22 01:02:02 -0300

    Update AdobeHDS.php

--- AdobeHDS.php
+++ AdobeHDS.php
@@ -1,4 +1,5 @@
-<?php
+<?php http://flacvcms-f.akamaihd.net/z/video/FLAC_FOD_LATAM/85/11/FOX_Fit_1_OK,.,__513641.,__363475.,__148033.,__480208.,__870848.,mp4.csmil/manifest.f4m
+
   define('AUDIO', 0x08);
   define('VIDEO', 0x09);
   define('AKAMAI_ENC_AUDIO', 0x0A);
